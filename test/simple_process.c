25db3e81dd7cfdbfd61073c84ab2c06ad50ef819
高小川 <gaoxiaochuan@hotmail.com>
2018-12-17 07:25:24 +0800

    test process register

---
+++ test/simple_process.c
@@ -0,0 +1,23 @@
+#include "cargs_themes.h"
+#include <stdio.h>
+
+cargs_process(test_process, int a, int b)
+{
+    printf("%d\n", a + b);
+}
+
+
+typedef void (*void_fptr_int_int)(int, int);
+
+int main()
+{
+    cargs_processes_init();
+
+    const struct __cargs_theme *theme = cargs_find_process("test_process");
+
+    void_fptr_int_int fptr = (void_fptr_int_int) theme->fptr;
+
+    fptr(255, 245);
+
+    return 0;
+}
