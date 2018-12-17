3ea7c6ab636c77c3fef0bb19c7ce5c2e499aebe2
高小川 <gaoxiaochuan@hotmail.com>
2018-12-17 12:32:05 +0800

    test arg

--- test/simple_arg.c
+++ test/simple_arg.c
@@ -5,22 +5,27 @@ cargs_flag(simple_arg_flag1, __SET__("flag1", "flag2", "flag3") );
 cargs_flag(simple_arg_flag2, __SET__("flag1", "flag4"));
 cargs_flag(simple_arg_flag3, __SET__("flag1"));
 cargs_flag(simple_arg_flag4, __SET__("flag2"));
+cargs_subflag(simple_arg_flag2, simple_arg_flag5, __SET__("flag5"));
 
-int main()
+int main(int argc, char **argv)
 {
     struct cargs_llnode *head;
     struct cargs_llnode *p;
-    int i = 0;
+    bool transfer_success;
 
+    cargs_arg_type_init();
     cargs_args_init();
 
     head = cargs_args_find("flag1");
 
-    for (p = head->next; p != head; p = p->next) {
-        i++;
-    }
+    transfer_success = cargs_transfer(argc, argv);
+    printf("%d\n", transfer_success);
 
-    printf("%d\n", i);
+    for (p = head->next; p != head; p = p->next) {
+        if (ll_entry(p, struct __cargs_arg_llnode, node)->arg->enable) {
+            printf("%s\n", ll_entry(p, struct __cargs_arg_llnode, node)->arg->_inner_name);
+        }
+    }
 
     return 0;
 }
